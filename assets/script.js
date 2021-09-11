b5fa11c19582739f9442eca8ec5ff9fa96cce24b
Ben Kaan <benkaan001@gmail.com>
2021-09-11 16:46:47 -0500

    assign classes and build jQuery CSS assignment based on presentTime

--- assets/script.js
+++ assets/script.js
@@ -2,3 +2,32 @@
 var currentDay = moment().format('MMMM Do YYYY, h:mm:ss a');
 $("#currentDay").html(currentDay);
 
+// present time
+
+var presentTime = moment().hour();
+
+var displayTime;
+
+//jQuery document is ready function
+
+$(document).ready(function(){
+    console.log ("ready!");
+
+});
+
+//change the CSS color property based on time
+
+if (displayTime === presentTime) {
+    $().addClass("present");
+    $().removeClass("past");
+    $().removeClass("future");
+
+} else if ( displayTime < presentTime) {
+    $().addClass("past");
+    $().removeClass("present");
+    $().removeClass("future");
+} else ( displayTime > presentTime) {
+    $().addClass("future");
+    $().removeClass("present");
+    $().removeClass("past");
+}
